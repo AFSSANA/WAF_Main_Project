b264d48d8506f16566ff72f98180763acddbc637
Afssana <afssana20@gmail.com>
2026-02-26 10:12:35 +0530

    Ec2 instance created

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "fashion_waf";
+$user = "wafuser";      // NOT root
+$pass = "StrongPass123!"; // password you set earlier
+$db   = "waf_project";  // correct database name
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Database connection failed");
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
